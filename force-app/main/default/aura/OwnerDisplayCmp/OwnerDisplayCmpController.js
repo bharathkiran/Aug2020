de2154af08963886e4aa8f1d7e27210e69b4a694
Bharath Ammanamanchi <bharathkiran@gmail.com>
2020-08-17 11:17:33 -0400

    First Commit

---
+++ force-app/main/default/aura/OwnerDisplayCmp/OwnerDisplayCmpController.js
@@ -0,0 +1,30 @@
+({
+    doInit : function(component, event, helper) {
+        var action = component.get("c.fetchOwnerDetail");
+        action.setParams({
+            "inId": component.get("v.recordId")
+        });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var res = response.getReturnValue();
+                component.set('v.oUser', res);
+            }
+            else if (state === "INCOMPLETE") {
+                // do something
+            }
+                else if (state === "ERROR") {
+                    var errors = response.getError();
+                    if (errors) {
+                        if (errors[0] && errors[0].message) {
+                            console.log("Error message: " + 
+                                        errors[0].message);
+                        }
+                    } else {
+                        console.log("Unknown error");
+                    }
+                }
+        });
+        $A.enqueueAction(action);	
+    },
+})
